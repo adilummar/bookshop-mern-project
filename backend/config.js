727f33860d124e9d46d3e4d1c9a19a5f7bd50465
adilummar <madilummer@gmail.com>
2023-11-28 14:41:45 +0530

    mongoose added

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 3001;
+
+export const mongoDBURL = 'mongodb+srv://adilummer:adilummer224@cluster0.sc2xgep.mongodb.net/?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
 import express  from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express()
+
+app.get('/', (req,res)=>{
+    return res.send("welcome to MERN app ")
+})
+
+
+mongoose.connect(mongoDBURL)
+.then(()=> {console.log("DB is successfuly connected");
 app.listen(PORT, (err)=>{
     if(err){
         console.log("there is something fishy")
     }else{
         console.log("port is running")
     }
-} )
+})
+}).catch((error)=>{
+    console.log(error)
+})
