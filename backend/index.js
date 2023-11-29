47802a1b731d01ed85635fc1ab1f8a7a72a1fb9d
adilummar <madilummer@gmail.com>
2023-11-29 09:38:24 +0530

    db post and get setted

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,48 @@
 import express  from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express()
+
+app.use(express.json())
 
 app.get('/', (req,res)=>{
     return res.send("welcome to MERN app ")
 })
 
+app.post('/books',async(req,res)=>{
+    try{if(
+        !req.body.title||
+        !req.body.author||
+        !req.body.publishYear
+    ){
+        return res.status(400).send({message:"send all required files: title,author, publishedYear",})
+    }
+
+    const newBook = {
+        title:req.body.title,
+        author:req.body.author,
+        publishYear:req.body.publishYear
+    }
+    const book = await Book.create(newBook)
+    return res.status(201).send(book)
+
+    }catch(error){
+        console.log(error.message);
+        res.status(500).send({message:error.message})
+    }
+})
+// get all book list
+app.get('/books',async(req,res)=>{
+    try{
+         const books = await Book.find({});
+         return res.status(200).json(books);
+    }catch(error){
+        console.log(error.message);
+        res.status(500).send({message:error.message})
+    }
+})
 
 mongoose.connect(mongoDBURL)
 .then(()=> {console.log("DB is successfuly connected");
